419517cf04ed3a9cfa038ebf18263dc6dcfae9a5
Stefan Hess <StefanHess@wohnbau-hess.de>
2026-08-01 23:37:25 +0200

    chore: bump app version to 1.0.5 [deploy]

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const dateTimeDE = new Intl.DateTimeFormat("de-DE", {
   timeStyle: "short",
 });
 const dateDE = new Intl.DateTimeFormat("de-DE", { dateStyle: "short" });
-const APP_VERSION = "v1.0.4";
+const APP_VERSION = "v1.0.5";
 const CURRENCY_OPTIONS = ["EUR", "TRY", "USD", "GBP", "CHF", "SEK", "NOK", "DKK", "PLN", "CZK", "HUF"];
 const CURRENCY_SYMBOL = { EUR: "€", TRY: "₺", USD: "$", GBP: "£", CHF: "Fr", SEK: "kr", NOK: "kr", DKK: "kr", PLN: "zł", CZK: "Kč", HUF: "Ft" };
 const AUTH_EMAIL_STORAGE_KEY = "bonbox_auth_email";
